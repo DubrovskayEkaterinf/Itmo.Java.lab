d7233a2458f447764d1b10d4db1265dcb9c6f01f
Dubrovskay Ekaterina <153015926+DubrovskayEkaterinf@users.noreply.github.com>
2023-12-17 16:48:26 +0300

    laboratory2

--- src/laboratory2/CalCul.java
+++ src/laboratory2/CalCul.java
@@ -3,59 +3,55 @@ package laboratory2;
 import java.util.Scanner;
 
 public class CalCul {
-    public static void main(String[] args) {
-        double num1;
-        double num2;
-        double answer;
-        String operation;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        num1 = Double.parseDouble(scanner.next());
-        System.out.print("Введите второе число: ");
-        num2 = Double.parseDouble(scanner.next());
-        System.out.print("Введите действие (+, -, *, /): ");
-        operation = scanner.nextLine();
-        switch (operation) {
-            case "+":
-                System.out.println(addition(num1, num2));
-                break;
-            case "-":
-                System.out.println(subtraction(num1, num2));
-                break;
-            case "*":
-                System.out.println(multiplication(num1, num2));
-                break;
-            case "/":
-                System.out.println(division(num1, num2));
-                break;
-            default:
-                System.out.print("Ошибка");
-                     }
-    }
 
     public static double addition(double num1, double num2) {
-        double answer = (num1 + num2);
-        return answer;
+        return num1 + num2;
+    }
+
+    public static long addition(long num1, long num2) {
+        return num1 + num2;
+    }
+
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
     }
 
     public static double subtraction(double num1, double num2) {
-        double answer = (num1 - num2);
-        return answer;
+        return num1 - num2;
+    }
+
+    public static long subtraction(long num1, long num2) {
+        return num1 - num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
     }
 
     public static double multiplication(double num1, double num2) {
-        double answer = (num1 * num2);
-        return answer;
+        return num1 * num2;
+    }
+
+    public static long multiplication(long num1, long num2) {
+        return num1 * num2;
+    }
+
+    public static double multiplication(int num1, int num2) {
+        return num1 * num2;
     }
 
     public static double division(double num1, double num2) {
-        double answer = (num1 / num2);
-        return answer;
+        return num1 / num2;
+    }
+
+    public static long division(long num1, long num2) {
+        return num1 / num2;
+    }
+
+    public static int division(int num1, int num2) {
+        return num1 / num2;
     }
 }
-
-
-
 
 
 
